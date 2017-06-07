b53c025e87022cd442f6b6c990f3351a810d77e0
Bonacci <jvh1234@outlook.com>
2017-06-07 15:37:23 +0200

    LemmatizorTest

--- src/main/java/guru/bonacci/oogway/sannyas/steps/Lemmatizator.java
+++ src/main/java/guru/bonacci/oogway/sannyas/steps/Lemmatizator.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -65,30 +64,5 @@ public class Lemmatizator implements Function<String,String> {
       }
     }
     return lemmas.stream().collect(joining(" "));
-  }
-
-  //TODO use for testing
-  public static void main(String[] args) {
-    System.out.println("Starting Stanford Lemmatizer");
-    String text = "How could you be seeing into my eyes like open doors? \n"+
-        "You led me down into my core where I've became so numb \n"+
-        "Without a soul my spirit's sleeping somewhere cold \n"+
-        "Until you find it there and led it back home \n"+
-        "You woke me up inside \n"+
-        "Called my name and saved me from the dark \n"+
-        "You have bidden my blood and it ran \n"+
-        "Before I would become undone \n"+
-        "You saved me from the nothing I've almost become \n"+
-        "You were bringing me to life \n"+
-        "Now that I knew what I'm without \n"+
-        "You can've just left me \n"+
-        "You breathed into me and made me real \n"+
-        "Frozen inside without your touch \n"+
-        "Without your love, darling \n"+
-        "Only you are the life among the dead \n"+
-        "I've been living a lie, there's nothing inside \n"+
-        "You were bringing me to life.";
-    Lemmatizator slem = new Lemmatizator();
-    System.out.println(slem.apply(text));
   }
 }

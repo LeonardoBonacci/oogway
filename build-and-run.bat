6967bbaf8736e9b42954aca549bfc50e0323e9ed
Bonacci <jvh1234@outlook.com>
2017-06-16 12:34:20 +0200

    prjoect restructuring reverted

--- build-and-run.bat
+++ build-and-run.bat
@@ -1,10 +1,9 @@
 @echo off
 title Simple way to build and run oogway, enjoy a coffee while you're waiting...
-cd..\..\..\..
+rem cd..\..\..\..
 echo Let's build oogway from directory %cd%
 
 set version=1.0.0-SNAPSHOT
-set parent=parent
 set persistence=oracle
 set registration=eureka
 set crawler=sannya
@@ -20,9 +19,9 @@ if "%skip-tests%" == "skip" (
 rem quickly build the parent pom
 start /wait cmd.exe /c "%maven-build%"
  
-start cmd.exe /k "cd %parent%\%registration% & java -jar target\\%registration%-%version%.jar""
-start cmd.exe /k "cd %parent%\%crawler% & java -jar target\\%crawler%-%version%.jar""
-start cmd.exe /k "cd %parent%\%web% & java -jar target\\%web%-%version%.jar""
+start cmd.exe /k "cd %registration% & java -jar target\\%registration%-%version%.jar""
+start cmd.exe /k "cd %crawler% & java -jar target\\%crawler%-%version%.jar""
+start cmd.exe /k "cd %web% & java -jar target\\%web%-%version%.jar""
 
 rem and return to the scripts directory
-cd parent\src\main\scripts
+gem cd parent\src\main\scripts

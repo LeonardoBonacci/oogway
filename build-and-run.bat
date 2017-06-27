0b8d1a17c5ae111e3132d0c96f9311bca0bd3817
Bonacci <jvh1234@outlook.com>
2017-06-28 00:19:41 +0200

    oracle as a separate module

--- build-and-run.bat
+++ build-and-run.bat
@@ -6,7 +6,7 @@ echo Let's build oogway from directory %cd%
 set version=1.0.0-SNAPSHOT
 set persistence=oracle
 set registration=eureka
-set oracle=oracle
+set oracle=oracle-service
 set sannya=sannya
 set web=web
 
@@ -21,7 +21,7 @@ rem quickly build the parent pom
 start /wait cmd.exe /c "%maven-build%"
  
 start cmd.exe /k "cd %registration% & java -jar target\\%registration%-%version%.jar""
-start cmd.exe /k "cd %oracle% & java -jar target\\%oracle%-%version%.jar""
+start cmd.exe /k "cd oracle\\%oracle% & java -jar target\\%oracle%-%version%.jar""
 start cmd.exe /k "cd %sannya% & java -jar target\\%sannya%-%version%.jar""
 start cmd.exe /k "cd %web% & java -jar target\\%web%-%version%.jar""
 

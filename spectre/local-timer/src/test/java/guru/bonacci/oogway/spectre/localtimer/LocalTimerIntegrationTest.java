fa932816bc308af8bc8eb58497b7fcd682aeead5
WhoAm2 <jvh1234@outlook.com>
2017-08-12 13:44:30 +0200

    localtimerintegrationtest: assert missing

--- spectre/local-timer/src/test/java/guru/bonacci/oogway/spectre/localtimer/LocalTimerIntegrationTest.java
+++ spectre/local-timer/src/test/java/guru/bonacci/oogway/spectre/localtimer/LocalTimerIntegrationTest.java
@@ -10,6 +10,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,14 +24,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.stream.binding.BinderAwareChannelResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
@@ -36,14 +35,13 @@ import org.springframework.web.client.RestTemplate;
 import guru.bonacci.oogway.spectre.localtimer.events.LocalTimerEventChannels;
 import guru.bonacci.oogway.spectre.secretpersistence.Spec;
 import guru.bonacci.oogway.spectre.secretpersistence.SpecRepository;
-import guru.bonacci.oogway.spectre.secretpersistence.SpecRepositoryCustom;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT, properties = {
 	"geo.name.username=voldemort"		
 })
 //A little hack to avoid creating profiles at this moment :)
-//The test resource property overrides some of the secret-persisence.properties that is
+//The test resource property overrides some of the secret-persistence.properties that is
 //read by the default configuration SecretPersistenceConfig
 @TestPropertySource("classpath:secret-persistence-test.properties")
 public class LocalTimerIntegrationTest {
@@ -55,9 +53,6 @@ public class LocalTimerIntegrationTest {
 
 	@Autowired 
 	SpecRepository repo;
-
-	@Autowired 
-	TestSpecRepository testRepo;
 
 	@MockBean
 	RestTemplate rest;
@@ -88,8 +83,7 @@ public class LocalTimerIntegrationTest {
 		String body = "{\"content\":\"" + uuid + "\"}";
 		sendMessage(body, LocalTimerEventChannels.ENRICHMENT, "application/json");
 
-		TestSpec persisted = testRepo.findOne(uuid);
-		System.out.println(persisted);
+		//TODO retrieve and check value
 	}
 
 	private void sendMessage(String body, String target, Object contentType) {
@@ -102,17 +96,6 @@ public class LocalTimerIntegrationTest {
 		return new DirectChannel();
 	}
 
-	@Repository
-	public interface TestSpecRepository extends ElasticsearchRepository<TestSpec, String> {
-	}
-
-	public class TestSpec extends Spec {
-		
-		@Field(type = FieldType.String)
-		public String test;
-
-	}
-	
 	@SpringBootApplication
 	public static class TestApp {
 

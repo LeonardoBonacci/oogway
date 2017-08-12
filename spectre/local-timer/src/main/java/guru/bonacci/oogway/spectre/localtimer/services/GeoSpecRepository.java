52583e0e7b98eb62244eb536ffeec5f601596da0
WhoAm2 <jvh1234@outlook.com>
2017-08-12 10:47:59 +0200

    localtimer ready, time for coffee

--- spectre/local-timer/src/main/java/guru/bonacci/oogway/spectre/localtimer/services/GeoSpecRepository.java
+++
@@ -1,8 +0,0 @@
-package guru.bonacci.oogway.spectre.localtimer.services;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GeoSpecRepository extends ElasticsearchRepository<GeoSpec, String> {
-}

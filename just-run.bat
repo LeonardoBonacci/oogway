0810ff74a444e498fb6bb9332d5a5635ce38d6dc
WhoAm2 <jvh1234@outlook.com>
2017-11-05 12:48:24 +0100

    spectre separated from oogway

--- just-run.bat
+++ just-run.bat
@@ -7,8 +7,6 @@ set orchestration=orchestration
 set oracle=oracle-service
 set sannya=sannya-service
 set web=web
-set localtimer=local-timer
-set sentiment=sentiment
 set jobs=jobs
 
 start cmd.exe /k "cd %orchestration% & java -jar target\%orchestration%-%version%.jar""
@@ -16,6 +14,4 @@ start cmd.exe /k "cd oracle\%oracle% & java -jar target\%oracle%-%version%.jar""
 start cmd.exe /k "cd sannya\%sannya% & java -jar target\%sannya%-%version%.jar""
 start cmd.exe /k "cd %web% & java -jar target\%web%-%version%.jar""
 start cmd.exe /k "cd %jobs% & java -jar target\%jobs%-%version%.jar""
-start cmd.exe /k "cd spectre\%localtimer% & java -jar target\%localtimer%-%version%.jar""
-start cmd.exe /k "cd spectre\%sentiment% & java -jar target\%sentiment%-%version%.jar""
 

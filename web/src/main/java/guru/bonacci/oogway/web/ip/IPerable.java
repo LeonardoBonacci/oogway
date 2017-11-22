9046d3bf245ddf4c9ae218ce0dd44ece79921f0a
WhoAm2 <jvh1234@outlook.com>
2017-11-22 10:09:22 +0100

    IPCrawler in IPerable

--- web/src/main/java/guru/bonacci/oogway/web/ip/IPerable.java
+++ web/src/main/java/guru/bonacci/oogway/web/ip/IPerable.java
@@ -1,13 +1,19 @@
 package guru.bonacci.oogway.web.ip;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -81,5 +87,30 @@ public class IPerable implements Iterable<String> {
 			}
 		};
 		return ips;
+	}
+	
+	@Component
+	public class IPCrawler {
+
+		public static final String serviceURL = "http://sqa.fyicenter.com/Online_Test_Tools/Test_IP_Address_Generator.php";
+
+		private static final int DEFAULT_SIZE = 12;
+
+		public List<String> crawl() throws IOException {
+			return crawl(DEFAULT_SIZE);
+		}
+		
+		public List<String> crawl(int count) throws IOException {
+	        Response response = 
+	                Jsoup.connect(serviceURL)
+	                .userAgent("Mozilla")
+	                .method(Method.POST)
+	                .data("Count", Integer.toString(count))
+	                .data("Submit", "Start")
+	                .execute();
+	        
+			Element textarea = response.parse().select("textarea").first();
+			return Stream.of(textarea.text().split("\n")).collect(toList());
+		}
 	}
 }

43be5a676d5b5f12108ad0d73d6729a8826b827d
WhoAm2 <jvh1234@outlook.com>
2018-01-24 17:32:38 +0100

    some runscript details

--- just-run.bat
+++ just-run.bat
@@ -3,9 +3,9 @@ title Run oogway...
 
 set version=2.2-SNAPSHOT
 
-start cmd.exe /k "cd orchestration & java -jar target\orchestration-%version%.jar""
-start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service-%version%.jar""
-start cmd.exe /k "cd sannya\sannya-service & java -jar target\sannya-service-%version%.jar""
+start cmd.exe /k "cd orchestration & java -jar target\orchestration-%version%.jar"
+start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service-%version%.jar"
+start cmd.exe /k "cd sannya\sannya-service & java -jar target\sannya-service-%version%.jar 2223"
 start cmd.exe /k "cd web & java -jar target\web-%version%.jar""
 start cmd.exe /k "cd jobs & java -jar target\jobs-%version%.jar""
 
--- spectre/just-run.bat
+++ spectre/just-run.bat
@@ -3,8 +3,8 @@ title Run spectre...
 
 set version=2.2-SNAPSHOT
 
-start cmd.exe /k "cd local-timer & java -jar target\local-timer-%version%.jar""
-start cmd.exe /k "cd sentiment & java -jar target\sentiment-%version%.jar""
-start cmd.exe /k "cd weather & java -jar target\weather-%version%.jar""
-start cmd.exe /k "cd money & java -jar target\money-%version%.jar""
+start cmd.exe /k "cd local-timer & java -jar target\local-timer-%version%.jar"
+start cmd.exe /k "cd sentiment & java -jar target\sentiment-%version%.jar"
+start cmd.exe /k "cd weather & java -jar target\weather-%version%.jar"
+start cmd.exe /k "cd money & java -jar target\money-%version%.jar"
 

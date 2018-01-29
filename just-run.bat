a7dc7d61bc14f03927be01f5305e6b74eb161e1e
WhoAm2 <jvh1234@outlook.com>
2018-01-29 18:01:46 +0100

    update version run script

--- just-run.bat
+++ just-run.bat
@@ -1,7 +1,7 @@
 @echo off
 title Run oogway...
 
-set version=2.2
+set version=2.3-SNAPSHOT
 
 start cmd.exe /k "cd orchestration & java -jar target\orchestration-%version%.jar"
 start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service-%version%.jar"

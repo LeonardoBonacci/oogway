1cc9035efcd1f19afdad380067d9b0d69e178bf4
WhoAm2 <jvh1234@outlook.com>
2018-01-31 08:19:02 +0100

    tor does not run by default

--- run-env.bat
+++ run-env.bat
@@ -6,13 +6,12 @@ echo We're working from directory %HOME%
 
 set ES_HOME=C:\Tools\elasticsearch-2.4.4
 set RABBIT_HOME=C:\Program Files\RabbitMQ Server
-rem set KIBANA_HOME=C:\Tools\kibana-4.6.3-windows-x86
+set KIBANA_HOME=C:\Tools\kibana-4.6.3-windows-x86
 set TOR_HOME=C:\Tools\Tor Browser\Browser\TorBrowser\Tor
 
 start cmd.exe /k "cd %ES_HOME% & bin\elasticsearch"
 start cmd.exe /k "cd %RABBIT_HOME% & rabbitmq_server-3.6.12\sbin\rabbitmq-server.bat"
 rem start cmd.exe /k "cd %KIBANA_HOME% & bin\kibana"
-
-start cmd.exe /k "cd %TOR_HOME% & tor.exe"
+rem start cmd.exe /k "cd %TOR_HOME% & tor.exe"
 
 call spectre\run-env.bat

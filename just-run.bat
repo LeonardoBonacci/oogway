b0dc4e63016be6e27c5a821cdc5843da4b1f7e24
WhoAm2 <jvh1234@outlook.com>
2018-02-02 08:04:52 +0100

    jar-names without version number

--- just-run.bat
+++ just-run.bat
@@ -1,16 +1,14 @@
 @echo off
 title Run oogway...
 
-set version=2.3-SNAPSHOT
-
 start cmd.exe /k "cd config & java -jar target\config.jar"
 
 TIMEOUT /T 5
 
-start cmd.exe /k "cd orchestration & java -jar target\orchestration-%version%.jar"
-start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service-%version%.jar"
-start cmd.exe /k "cd sannya\sannya-service & java -jar target\sannya-service-%version%.jar 2223"
-start cmd.exe /k "cd web & java -jar target\web-%version%.jar""
-start cmd.exe /k "cd jobs & java -jar target\jobs-%version%.jar""
+start cmd.exe /k "cd orchestration & java -jar target\orchestration.jar"
+start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service.jar"
+start cmd.exe /k "cd sannya\sannya-service & java -jar target\sannya-service.jar"
+start cmd.exe /k "cd web & java -jar target\web.jar"
+start cmd.exe /k "cd jobs & java -jar target\jobs.jar""
 
 start cmd.exe /k "cd monitoring & mvn spring-boot:run"

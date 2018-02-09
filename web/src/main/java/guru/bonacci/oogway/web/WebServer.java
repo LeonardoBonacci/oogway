f8556a3831249f99446f2022a8f7bb1be46e7474
WhoAm2 <jvh1234@outlook.com>
2018-02-09 08:22:13 +0100

    web properties all moved to config server yml's

--- web/src/main/java/guru/bonacci/oogway/web/WebServer.java
+++ web/src/main/java/guru/bonacci/oogway/web/WebServer.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 
 import guru.bonacci.oogway.oracle.client.OracleClientConfig;
@@ -19,20 +18,9 @@ import guru.bonacci.oogway.web.events.WebEventChannels;
 @EnableBinding(WebEventChannels.class)
 @IntegrationComponentScan
 @Import(OracleClientConfig.class)
-@PropertySource("classpath:web.yml")
 public class WebServer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServer.class, args);
 	}
-
-//	//spring-boot doesn't support yml files
-//	@Bean
-//	public static PropertySourcesPlaceholderConfigurer properties() {
-//      PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-//      YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
-//      yaml.setResources(new ClassPathResource("web.yml"));
-//      propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
-//      return propertySourcesPlaceholderConfigurer;
-//  }
 }

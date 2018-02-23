f859cf76f2770d34b0f558b6297f449c3e1acaa4
WhoAm2 <jvh1234@outlook.com>
2018-02-23 04:51:07 +0100

    minor detail

--- spectre/run-env.bat
+++ spectre/run-env.bat
@@ -3,4 +3,4 @@ title Setup spectres environment...
 
 set LOGSTASH_HOME=C:\Tools\logstash-6.2.1
 
-start cmd.exe /k "cd %LOGSTASH_HOME% & bin\logstash --path.settings=%cd%\\logstash-settings
+start cmd.exe /k "cd %LOGSTASH_HOME% & bin\logstash --path.settings=%cd%\logstash-settings

384c1cbd369f77805820c40214ecc61a03fe3eb9
WhoAm2 <jvh1234@outlook.com>
2018-02-24 07:33:30 +0100

    auth config moved to config-server

--- just-run.bat
+++ just-run.bat
@@ -3,7 +3,7 @@ title Run oogway...
 
 rem set github configuration branch name as environment variable
 rem no value defaults to master branch 
-set CLOUD_CONFIG_FIND_LABEL=master
+set CLOUD_CONFIG_FIND_LABEL=secure
 
 start cmd.exe /k "cd config & java -jar target\config.jar"
 
@@ -12,6 +12,9 @@ TIMEOUT /T 15
 rem start cmd.exe /k "cd monitoring & java -jar target\monitoring.jar"
 
 start cmd.exe /k "cd orchestration & java -jar target\orchestration-service.jar"
+
+start cmd.exe /k "cd auth & java -jar target\auth-service.jar"
+
 start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service.jar"
 start cmd.exe /k "cd sannyas\sannyas-service & java -jar target\sannyas-service.jar"
 start cmd.exe /k "cd web & java -jar target\web-service.jar"

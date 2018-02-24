5b1ef6d649c83998c6da49d3a72cd3a71b595c08
WhoAm2 <jvh1234@outlook.com>
2018-02-24 18:39:04 +0100

    LS_HOME used in run script

--- spectre/run-env.bat
+++ spectre/run-env.bat
@@ -1,6 +1,4 @@
 @echo off
 title Setup spectres environment...
 
-set LOGSTASH_HOME=C:\Tools\logstash-6.2.1
-
-start cmd.exe /k "cd %LOGSTASH_HOME% & bin\logstash --path.settings=%cd%\logstash-settings
+start cmd.exe /k "cd %LS_HOME% & bin\logstash --path.settings=%cd%\logstash-settings

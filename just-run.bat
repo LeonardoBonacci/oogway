8be343345068cb4b6159b7c8e7e3af05ee363cb3
WhoAm2 <jvh1234@outlook.com>
2018-02-26 06:20:21 +0100

    heimdall logs in ES

--- just-run.bat
+++ just-run.bat
@@ -7,18 +7,20 @@ set CLOUD_CONFIG_FIND_LABEL=zuul
 
 start cmd.exe /k "cd config & java -jar target\config.jar"
 
-TIMEOUT /T 10
+TIMEOUT /T 15
 
 start cmd.exe /k "cd orchestration & java -jar target\orchestration-service.jar"
 
-rem start cmd.exe /k "cd auth & java -jar target\auth-service.jar"
+start cmd.exe /k "cd auth & java -jar target\auth-service.jar"
 
-rem start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service.jar"
-rem start cmd.exe /k "cd sannyas\sannyas-service & java -jar target\sannyas-service.jar"
+start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service.jar"
+start cmd.exe /k "cd sannyas\sannyas-service & java -jar target\sannyas-service.jar"
 start cmd.exe /k "cd web & java -jar target\web-service.jar"
 
-rem start cmd.exe /k "cd jobs & java -jar target\job-service.jar"
+start cmd.exe /k "cd jobs & java -jar target\job-service.jar"
 
-rem TIMEOUT /T 30
+start cmd.exe /k "cd heimdall & java -jar target\heimdall.jar"
 
-rem start cmd.exe /k "cd zipkin & java -jar target\zipkin.jar"
+TIMEOUT /T 30
+
+start cmd.exe /k "cd zipkin & java -jar target\zipkin.jar"

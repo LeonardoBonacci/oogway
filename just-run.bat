65e701f973fe8f60aea03729d511690c6760d680
WhoAm2 <jvh1234@outlook.com>
2018-02-26 02:43:02 +0100

    test eureka server removed

--- just-run.bat
+++ just-run.bat
@@ -7,18 +7,18 @@ set CLOUD_CONFIG_FIND_LABEL=master
 
 start cmd.exe /k "cd config & java -jar target\config.jar"
 
-TIMEOUT /T 15
+TIMEOUT /T 5
 
 start cmd.exe /k "cd orchestration & java -jar target\orchestration-service.jar"
 
-start cmd.exe /k "cd auth & java -jar target\auth-service.jar"
+rem start cmd.exe /k "cd auth & java -jar target\auth-service.jar"
 
-start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service.jar"
-start cmd.exe /k "cd sannyas\sannyas-service & java -jar target\sannyas-service.jar"
-start cmd.exe /k "cd web & java -jar target\web-service.jar"
+rem start cmd.exe /k "cd oracle\oracle-service & java -jar target\oracle-service.jar"
+rem start cmd.exe /k "cd sannyas\sannyas-service & java -jar target\sannyas-service.jar"
+rem start cmd.exe /k "cd web & java -jar target\web-service.jar"
 
-start cmd.exe /k "cd jobs & java -jar target\job-service.jar"
+rem start cmd.exe /k "cd jobs & java -jar target\job-service.jar"
 
-TIMEOUT /T 30
+rem TIMEOUT /T 30
 
-start cmd.exe /k "cd zipkin & java -jar target\zipkin.jar"
+rem start cmd.exe /k "cd zipkin & java -jar target\zipkin.jar"

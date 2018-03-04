1911dcf31dbef498ea8739a213ada26e04a9ddab
WhoAm2 <jvh1234@outlook.com>
2018-03-04 07:17:22 +0100

    es runs as docker container. it's an unsupported old image (2.4.6) that sometimes crashes at oracle-service's start-up

--- run-env.bat
+++ run-env.bat
@@ -4,12 +4,11 @@ title Setup oogways environment...
 set HOME=%cd%
 echo We're working from directory %HOME%
 
-set ES_HOME=C:\Tools\elasticsearch-2.4.4
 set RABBIT_HOME=C:\Program Files\RabbitMQ Server
 set KIBANA_HOME=C:\Tools\kibana-4.6.3-windows-x86
 set TOR_HOME=C:\Tools\Tor Browser\Browser\TorBrowser\Tor
 
-start cmd.exe /k "cd %ES_HOME% & bin\elasticsearch"
+docker run -d -it -p 9200:9200 -p 9300:9300 elasticsearch:2.4.6
 start cmd.exe /k "cd %RABBIT_HOME% & rabbitmq_server-3.6.12\sbin\rabbitmq-server.bat"
 rem start cmd.exe /k "cd %KIBANA_HOME% & bin\kibana"
 rem start cmd.exe /k "cd %TOR_HOME% & tor.exe"

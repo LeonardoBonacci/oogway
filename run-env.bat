7867f7bf9eed01faa6d343d806e073804c841471
WhoAm2 <jvh1234@outlook.com>
2018-03-04 08:40:38 +0100

    rabbit dockers

--- run-env.bat
+++ run-env.bat
@@ -4,12 +4,11 @@ title Setup oogways environment...
 set HOME=%cd%
 echo We're working from directory %HOME%
 
-set RABBIT_HOME=C:\Program Files\RabbitMQ Server
 set KIBANA_HOME=C:\Tools\kibana-4.6.3-windows-x86
 set TOR_HOME=C:\Tools\Tor Browser\Browser\TorBrowser\Tor
 
 docker run -d -it -p 9200:9200 -p 9300:9300 elasticsearch:2.4.6
-start cmd.exe /k "cd %RABBIT_HOME% & rabbitmq_server-3.6.12\sbin\rabbitmq-server.bat"
+docker run -d -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 rabbitmq:3.6-management
 rem start cmd.exe /k "cd %KIBANA_HOME% & bin\kibana"
 rem start cmd.exe /k "cd %TOR_HOME% & tor.exe"
 

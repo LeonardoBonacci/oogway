695c8ed97f7dc6a426eb8e369067dec31c87a1f5
WhoAm2 <jvh1234@outlook.com>
2018-03-05 08:19:51 +0100

    kibana meets docker..

--- run-env.bat
+++ run-env.bat
@@ -1,15 +1,13 @@
 @echo off
 title Setup oogways environment...
 
-set HOME=%cd%
-echo We're working from directory %HOME%
+echo We're working from directory %cd%
 
-set KIBANA_HOME=C:\Tools\kibana-4.6.3-windows-x86
 set TOR_HOME=C:\Tools\Tor Browser\Browser\TorBrowser\Tor
 
-docker run -d -it -p 9200:9200 -p 9300:9300 elasticsearch:2.4.6
-docker run -d -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 rabbitmq:3.6-management
-rem start cmd.exe /k "cd %KIBANA_HOME% & bin\kibana"
+docker run -d --name es -p 9200:9200 -p 9300:9300 elasticsearch:2.4
+docker run -d --name rabbit -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 rabbitmq:3.6-management
+docker run -d --name kibana --link es:elasticsearch -p 5601:5601 kibana:4.6
 rem start cmd.exe /k "cd %TOR_HOME% & tor.exe"
 
 rem run spectre from its own directory

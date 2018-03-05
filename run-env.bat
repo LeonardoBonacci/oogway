b282d2ecef322672b4181ccaefdfa6e8e8dc1849
WhoAm2 <jvh1234@outlook.com>
2018-03-05 09:10:31 +0100

    docker tor

--- run-env.bat
+++ run-env.bat
@@ -3,12 +3,10 @@ title Setup oogways environment...
 
 echo We're working from directory %cd%
 
-set TOR_HOME=C:\Tools\Tor Browser\Browser\TorBrowser\Tor
-
 docker run -d --name es -p 9200:9200 -p 9300:9300 elasticsearch:2.4
 docker run -d --name rabbit -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 rabbitmq:3.6-management
 docker run -d --name kibana --link es:elasticsearch -p 5601:5601 kibana:4.6
-rem start cmd.exe /k "cd %TOR_HOME% & tor.exe"
+docker run -d --name tor -p 127.0.0.1:9050:9050 osminogin/tor-simple
 
 rem run spectre from its own directory
 cd spectre 

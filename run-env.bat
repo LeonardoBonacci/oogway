c42f41bd483af31fbebd9032f545637bdc804cf5
WhoAm2 <jvh1234@outlook.com>
2018-03-06 06:40:56 +0100

    logstash step 1

--- run-env.bat
+++ run-env.bat
@@ -6,9 +6,5 @@ echo We're working from directory %cd%
 docker run -d --name es -p 9200:9200 -p 9300:9300 elasticsearch:2.4
 docker run -d --name rabbit -p 4369:4369 -p 5672:5672 -p 15672:15672 -p 25672:25672 rabbitmq:3.6-management
 docker run -d --name kibana --link es:elasticsearch -p 5601:5601 kibana:4.6
-docker run -d --name tor -p 127.0.0.1:9050:9050 osminogin/tor-simple
-
-rem run spectre from its own directory
-cd spectre 
-call run-env.bat
-cd.. 
+docker run --name logstash --link es:elasticsearch --rm --link rabbit:rabbitmq -it -v C:/Users/Leo/git/oogway/spectre/logstash-settings/empty:/usr/share/logstash/pipeline/ docker.elastic.co/logstash/logstash-oss:6.2.2
+rem docker run -d --name tor -p 127.0.0.1:9050:9050 osminogin/tor-simple

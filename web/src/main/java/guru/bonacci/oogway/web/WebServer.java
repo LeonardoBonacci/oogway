d22edbfcf7dfcb0e34cca1c48ef92b84528f5095
WhoAm2 <jvh1234@outlook.com>
2018-03-11 11:08:54 +0100

    web heimdall orchestration-service work as docker containers

--- web/src/main/java/guru/bonacci/oogway/web/WebServer.java
+++ web/src/main/java/guru/bonacci/oogway/web/WebServer.java
@@ -2,6 +2,7 @@ package guru.bonacci.oogway.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Import;
 import org.springframework.integration.annotation.IntegrationComponentScan;
@@ -13,6 +14,7 @@ import guru.bonacci.oogway.web.events.WebEventChannels;
  * Service for the user to communicate with
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableBinding(WebEventChannels.class)
 @IntegrationComponentScan
 @Import(OracleClientConfig.class)

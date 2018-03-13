48a51b4f2af81db11fb77c51d4545a84edbc4715
WhoAm2 <jvh1234@outlook.com>
2018-03-13 11:48:42 +0100

    ip interceptor includes localhost of docker

--- web/src/main/java/guru/bonacci/oogway/web/ip/IPologist.java
+++ web/src/main/java/guru/bonacci/oogway/web/ip/IPologist.java
@@ -17,17 +17,20 @@ public class IPologist implements IIPologist {
 
 	static final String LOCAL_IP_1 = "0:0:0:0:0:0:0:1";
 	static final String LOCAL_IP_2 = "127.0.0.1";
+	static final String DOCKER_IP_2 = "172.19.0.1";
 
 	private Iterator<String> iperator;
 
 	@Autowired
 	public IPologist(IPerable iperable) {
 		iperator = iperable.iterator();
-	}	
+	}
 
 	@Override
 	public String checkUp(String ipIn) {
-		String ipOut = ipIn == null || LOCAL_IP_1.equals(ipIn) || LOCAL_IP_2.equals(ipIn) ? iperator.next() : ipIn;
+		String ipOut = ipIn == null || LOCAL_IP_1.equals(ipIn) || LOCAL_IP_2.equals(ipIn) || DOCKER_IP_2.equals(ipIn)
+				? iperator.next()
+				: ipIn;
 		logger.debug(ipIn + " becomes " + ipOut);
 		return ipOut;
 	}

771d3ca92f30dca6477286444db255780e211c13
WhoAm2 <jvh1234@outlook.com>
2018-03-24 15:43:15 +0100

    eureka client works

--- heimdall/src/main/java/guru/bonacci/oogway/heimdall/HeimdallApp.java
+++ heimdall/src/main/java/guru/bonacci/oogway/heimdall/HeimdallApp.java
@@ -2,9 +2,10 @@ package guru.bonacci.oogway.heimdall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HeimdallApp {
 
 	public static void main(String[] args) {

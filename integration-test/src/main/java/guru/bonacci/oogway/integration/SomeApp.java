13957c5127c0bd90cde3efdfec02ad017e681610
WhoAm2 <jvh1234@outlook.com>
2018-03-24 15:13:02 +0100

    second shot

--- integration-test/src/main/java/guru/bonacci/oogway/integration/SomeApp.java
+++ integration-test/src/main/java/guru/bonacci/oogway/integration/SomeApp.java
@@ -2,8 +2,12 @@ package guru.bonacci.oogway.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication()
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class SomeApp {
 
     public static void main(String[] args) {

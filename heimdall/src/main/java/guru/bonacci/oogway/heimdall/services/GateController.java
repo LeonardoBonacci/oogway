29ae4e07af9f29c171a77acb377df39158264bce
WhoAm2 <jvh1234@outlook.com>
2018-03-24 13:50:06 +0100

    heimdall it

--- heimdall/src/main/java/guru/bonacci/oogway/heimdall/services/GateController.java
+++ heimdall/src/main/java/guru/bonacci/oogway/heimdall/services/GateController.java
@@ -2,6 +2,7 @@ package guru.bonacci.oogway.heimdall.services;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GateController {
@@ -10,4 +11,9 @@ public class GateController {
 	public String ourOneAndOnlyPage() {
 	    return "html/the-html.html";
 	}
+
+	@RequestMapping("/version")
+    public @ResponseBody String greeting() {
+        return "Hello World";
+    }
 }

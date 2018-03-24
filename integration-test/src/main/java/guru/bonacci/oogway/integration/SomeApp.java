dfb2e166ac797d8e281ae1673cb1a514f1dfadfd
WhoAm2 <jvh1234@outlook.com>
2018-03-24 11:52:05 +0100

    run integration-tests module

--- integration-test/src/main/java/guru/bonacci/oogway/integration/SomeApp.java
+++ integration-test/src/main/java/guru/bonacci/oogway/integration/SomeApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SomeApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SomeApp.class, args);
+        SpringApplication.run(SomeTestApp.class, args);
     }
 }

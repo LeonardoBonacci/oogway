63a57d65aa4f8bb9c0500d25a25ad346135ffb0a
WhoAm2 <jvh1234@outlook.com>
2018-03-24 15:36:04 +0100

    run..

--- heimdall/src/main/java/guru/bonacci/oogway/heimdall/HeimdallApp.java
+++ heimdall/src/main/java/guru/bonacci/oogway/heimdall/HeimdallApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class HeimdallApp {
 
 	public static void main(String[] args) {

a47f284dc99907eaec586603ca8e3cc709394e75
WhoAm2 <jvh1234@outlook.com>
2018-03-31 13:43:26 +0200

    first version web-oracle communication throught passwords

--- auth/src/main/java/guru/bonacci/oogway/auth/AuthServer.java
+++ auth/src/main/java/guru/bonacci/oogway/auth/AuthServer.java
@@ -3,70 +3,73 @@ package guru.bonacci.oogway.auth;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.security.Principal;
+import java.util.Arrays;
+
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import guru.bonacci.oogway.auth.models.User;
+import guru.bonacci.oogway.auth.services.MyUserService;
+
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 @EnableResourceServer
+@EnableAsync
+@RestController
 public class AuthServer {
 
 	private final Logger logger = getLogger(this.getClass());
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
 	public static void main(String[] args) {
         SpringApplication.run(AuthServer.class, args);
     }
+
+	@Bean @Qualifier("mainDataSource")
+	public DataSource dataSource(){
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+				.setType(EmbeddedDatabaseType.H2)
+				.build();
+		return db;
+	}
+
+	@Bean
+	CommandLineRunner init(MyUserService accountService) {
+		return (evt) -> Arrays.asList(
+				"user1,user2".split(",")).forEach(
+				username -> {
+					User acct = new User();
+					acct.setUsername(username);
+					acct.setPassword("password");
+					accountService.registerUser(acct);
+				}
+		);
+	}
 
     @RequestMapping("/user")
     public Principal user(Principal user) {
         logger.info("AS /user has been called");
         logger.debug("user info: " + user.toString());
         return user;
-    }
-    
-    @Configuration
-    @EnableAuthorizationServer
-    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-
-        @Autowired
-        private AuthenticationManager authenticationManager;
-
-        @Override
-        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints.authenticationManager(authenticationManager);
-        }
-
-        @Override
-        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        	//TODO .secret(env.getProperty("WEB_SERVICE_PASSWORD"))
-        	// 		@Autowired private Environment env;
-
-			// @formatter:off
-        	clients.inMemory()
-		            .withClient("web-service")
-		            .secret("web-service-secret")
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-		            .scopes("resource-server-read", "resource-server-write")
-			.and()
-					.withClient("job-service")
-					.secret("job-service-secret")
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-		            .scopes("resource-server-read", "resource-server-write");
-			// @formatter:on
-        }
     }
 }

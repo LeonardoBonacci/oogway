601c368b2bee7482fb248a4a14f69cedb7dc2eff
WhoAm2 <jvh1234@outlook.com>
2018-04-02 13:15:19 +0200

    oauth client configured from config server

--- web/src/main/java/guru/bonacci/oogway/web/clients/OAuth2Config.java
+++ web/src/main/java/guru/bonacci/oogway/web/clients/OAuth2Config.java
@@ -9,16 +9,29 @@ import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 
+@RefreshScope
 @Configuration
 public class OAuth2Config {
 
 	public final static String OAUTH2_TEMPLATE_BEAN = "oAuth2RestTemplate";
 
+    @Value("${security.oauth2.client.accessTokenUri}")
+	private String accessTokenUri;
+
+	@Value("${security.oauth2.client.clientId}")
+	private String clientId;
+
+    @Value("${security.oauth2.client.clientSecret}")
+	private String clientSecret;
+    
+    @Value("${security.oauth2.client.grant-type}")
+	private String grantType;
+    
 	@RefreshScope
 	@Bean(OAUTH2_TEMPLATE_BEAN)
 	@Scope("prototype")
 	OAuth2RestTemplate restTemplate(@Value("${u:user1}") String username,
-										   @Value("${pw:password}") String pw) {
+								    @Value("${pw:password}") String pw) {
 		return new OAuth2RestTemplate(resourceDetails(pw, username), new DefaultOAuth2ClientContext());
 	}
 
@@ -26,14 +39,14 @@ public class OAuth2Config {
 	@Bean
 	@Scope("prototype")
 	ResourceOwnerPasswordResourceDetails resourceDetails(@Value("${u:user1}") String username,
-																@Value("${pw:password}") String pw) {
+														 @Value("${pw:password}") String pw) {
 		ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
 		resource.setUsername(username);
 		resource.setPassword(pw);
-		resource.setAccessTokenUri("http://auth-service:5000/auth/oauth/token");
-		resource.setClientId("web-service");
-		resource.setClientSecret("web-service-secret");
-		resource.setGrantType("password");
+		resource.setAccessTokenUri(accessTokenUri);
+		resource.setClientId(clientId);
+		resource.setClientSecret(clientSecret);
+		resource.setGrantType(grantType);
 		return resource;
 	}	
 }

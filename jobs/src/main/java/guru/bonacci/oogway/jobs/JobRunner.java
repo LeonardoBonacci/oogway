c6e45b3672059badf8bee2e6ce47fda3b978796c
WhoAm2 <jvh1234@outlook.com>
2018-04-14 15:10:59 +0200

    job accesstokenuri works

--- jobs/src/main/java/guru/bonacci/oogway/jobs/JobRunner.java
+++ jobs/src/main/java/guru/bonacci/oogway/jobs/JobRunner.java
@@ -1,19 +1,63 @@
 package guru.bonacci.oogway.jobs;
 
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import guru.bonacci.oogway.jobs.twitter.Tweeter;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@RestController
 public class JobRunner {
+
+	private final Logger logger = getLogger(this.getClass());
 
     public static void main(String[] args) {
         SpringApplication.run(JobRunner.class, args);
     }
+    
+    @Autowired
+    public Tweeter tweeter;
+    
+	@RequestMapping(value = "/tweet", method = GET)
+	public String tweet() {
+		logger.info("Manual tweet");
+		
+		tweeter.runForrestRun();
+		return "tweet tweet";
+	}	
+	
+	@Configuration
+	@EnableWebSecurity
+	protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+		    .authorizeRequests()
+			    .antMatchers("/**").permitAll()
+		    .and()
+		        .httpBasic();
+			// @formatter:on
+		}
+	}
+
 }
--- jobs/src/main/java/guru/bonacci/oogway/jobs/clients/ClientCredentialsGrantConfig.java
+++ jobs/src/main/java/guru/bonacci/oogway/jobs/clients/ClientCredentialsGrantConfig.java
@@ -2,15 +2,21 @@ package guru.bonacci.oogway.jobs.clients;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.AccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
-import feign.Logger;
 import feign.RequestInterceptor;
 
 @Configuration
@@ -18,29 +24,38 @@ import feign.RequestInterceptor;
 @Profile("!unit-test") // hack :)
 public class ClientCredentialsGrantConfig {
 
-	@Bean
-	Logger.Level feignLoggerLevel() {
-		return Logger.Level.FULL;
-	}
-
 	/**
 	 * A few small things are needed to secure a service..
 	 * 
 	 * The command line can pretend to be jobs-service: 
-	 * curl jobs-service:jobs-service-secret@localhost:5000/auth/oauth/token -d grant_type=client_credentials 
+	 * curl jobs-service:jobs-service-secret@localhost:5000/auth/oauth/token -d "grant_type=client_credentials" 
 	 * curl -H "Authorization: Bearer f8f016c2-184c-432f-8ee6-6613e7dbfdfd" -v http://localhost:4444/oracle/gems/random
 	 */
+	@Primary
 	@Bean
-	public OAuth2RestTemplate clientCredentialsRestTemplate() {
-		return new OAuth2RestTemplate(clientCredentialsResourceDetails());
+	@LoadBalanced
+	public RestTemplate clientCredentialsRestTemplate() {
+		OAuth2RestTemplate template = new OAuth2RestTemplate(clientCredentialsResourceDetails(), oAuth2ClientContext());
+		template.setAccessTokenProvider(accessTokenProvider());
+		return template;
+	}
+	
+	@Bean
+	public OAuth2ClientContext oAuth2ClientContext() {
+		return new DefaultOAuth2ClientContext();
 	}
 
-	/**
-	 * Spring Boot (1.4.1) does not create an OAuth2ProtectedResourceDetails
-	 * automatically if you are using client_credentials tokens. In that case you
-	 * need to create your own ClientCredentialsResourceDetails and configure it
-	 * with @ConfigurationProperties("security.oauth2.client").
-	 */
+	@Bean
+	public AccessTokenProvider accessTokenProvider() {
+		return new MyClientCredentialsAccessTokenProvider(loadBalancedTemplate());
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate loadBalancedTemplate() {
+		return new RestTemplate();
+	}
+	
 	@Bean
 	@ConfigurationProperties(prefix = "security.oauth2.client")
 	public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
@@ -49,6 +64,24 @@ public class ClientCredentialsGrantConfig {
 
 	@Bean
 	public RequestInterceptor oauth2FeignRequestInterceptor() {
-		return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
+		OAuth2FeignRequestInterceptor interceptor = new OAuth2FeignRequestInterceptor(oAuth2ClientContext(), clientCredentialsResourceDetails());
+		interceptor.setAccessTokenProvider(accessTokenProvider());
+		return interceptor;
+	}
+	
+	// Allows us to set a (loadbalanced) resttemplate
+	static class MyClientCredentialsAccessTokenProvider extends ClientCredentialsAccessTokenProvider {
+
+		private RestOperations restOperations;
+
+		public MyClientCredentialsAccessTokenProvider(RestOperations restOperations) {
+			this.restOperations = restOperations;
+		}
+
+		@Override
+		protected RestOperations getRestTemplate() {
+			setMessageConverters(new RestTemplate().getMessageConverters());
+			return this.restOperations;
+		}
 	}
 }

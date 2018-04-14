0a771169154b2a7fe8f416ca9739b16586db3e05
WhoAm2 <jvh1234@outlook.com>
2018-04-14 16:48:01 +0200

    loadbalanced customer user info token

--- oracle/src/main/java/guru/bonacci/oogway/oracle/service/OracleServer.java
+++ oracle/src/main/java/guru/bonacci/oogway/oracle/service/OracleServer.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +15,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.BaseOAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.client.RestTemplate;
 
 import guru.bonacci.oogway.oracle.service.events.OracleEventChannels;
 import guru.bonacci.oogway.oracle.service.security.CustomUserInfoTokenServices;
@@ -46,10 +50,19 @@ public class OracleServer extends ResourceServerConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(OracleServer.class, args);
 	}
+
+	@LoadBalanced
+	@Bean
+	public OAuth2RestTemplate loadBalancedTemplate() {
+		BaseOAuth2ProtectedResourceDetails resource = new BaseOAuth2ProtectedResourceDetails();
+		return new OAuth2RestTemplate(resource);
+	}
 	
 	@Bean
 	public ResourceServerTokenServices tokenServices() {
-		return new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
+		CustomUserInfoTokenServices serv = new CustomUserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
+		serv.setRestTemplate(loadBalancedTemplate());
+		return serv;
 	}
 
 	@Bean

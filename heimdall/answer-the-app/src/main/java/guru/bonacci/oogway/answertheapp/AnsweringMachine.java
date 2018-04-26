65e95ce7cbb8483ce4eb3cf1e532fd1b36577b7e
WhoAm2 <jvh1234@outlook.com>
2018-04-26 15:11:12 +0200

    few extra quotes from schopenhauer

--- heimdall/answer-the-app/src/main/java/guru/bonacci/oogway/answertheapp/AnsweringMachine.java
+++ heimdall/answer-the-app/src/main/java/guru/bonacci/oogway/answertheapp/AnsweringMachine.java
@@ -3,17 +3,19 @@ package guru.bonacci.oogway.answertheapp;
 import static guru.bonacci.oogway.utilities.CustomFileUtils.readToList;
 import static guru.bonacci.oogway.utilities.CustomListUtils.random;
 import static org.slf4j.LoggerFactory.getLogger;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import guru.bonacci.oogway.shareddomain.GemCarrier;
@@ -24,7 +26,7 @@ public class AnsweringMachine {
 
 	private final Logger logger = getLogger(this.getClass());
 
-	List<GemCarrier> friedrichsBest;
+	List<GemCarrier> answers;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnsweringMachine.class, args);
@@ -33,15 +35,21 @@ public class AnsweringMachine {
 	@PostConstruct
 	public void postEventum() {
 		try {
-			friedrichsBest = readToList("nietzsche.txt").stream()
-					.map(quote -> new GemCarrier(quote, "Friedrich Nietzsche")).collect(Collectors.toList());
+			answers = readToList("nietzsche.txt").stream()
+					.map(quote -> new GemCarrier(quote, "Friedrich Nietzsche")).collect(toList());
+			answers.addAll(readToList("schop.txt").stream()
+					.map(quote -> new GemCarrier(quote, "Arthur Schopenhauer")).collect(toList()));
 		} catch (IOException e) {
-			logger.error("Where is Nietzsche??", e);
+			logger.error("What just happened??", e);
 		}
 	}
 
-	@GetMapping("/greeting")
-	public GemCarrier greeting() {
-		return random(friedrichsBest).get(); // you may crash..
+	@GetMapping("/consult")
+	public GemCarrier greeting(@RequestParam("q") String q, @RequestParam("apikey") String apiKey) {
+		logger.info("incoming: " + q);
+
+		if (StringUtils.isBlank(q) || StringUtils.isBlank(apiKey))
+			throw new TheOnlyPossibleException();
+		return random(answers).get(); // you may crash..
 	}
 }

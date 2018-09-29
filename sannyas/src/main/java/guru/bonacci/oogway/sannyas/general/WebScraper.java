d5077f223b3ff58a594824d83e865f3d99aad501
WhoAm2 <jvh1234@outlook.com>
2018-09-29 05:40:16 +0200

    sannyas is fully reactive

--- sannyas/src/main/java/guru/bonacci/oogway/sannyas/general/WebScraper.java
+++ sannyas/src/main/java/guru/bonacci/oogway/sannyas/general/WebScraper.java
@@ -1,7 +1,7 @@
 package guru.bonacci.oogway.sannyas.general;
 
-import static java.util.Arrays.stream;
 import static java.util.Collections.synchronizedSet;
+import static java.util.function.Function.identity;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -38,13 +38,12 @@ public abstract class WebScraper implements Scraper {
 
 	@Override
 	public Flux<GemCarrier> find(String... tags) {
-		return Flux.fromStream(
-				stream(tags)
+		return Flux.fromArray(tags)
 					.map(this::determineURL)
 					.filter(consultedWebPages::add) //returns false when present in set
 					.map(this::consultWeb)
-					.flatMap(Elements::stream)
-					.map(this::toGem));
+					.flatMapIterable(identity())
+					.map(this::toGem);
 	}
 
 	/**

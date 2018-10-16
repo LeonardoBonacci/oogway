55e9e4929087b4da94840e0fff9af814f7e2fc16
WhoAm2 <jvh1234@outlook.com>
2018-10-16 15:17:28 +0200

    bug solved: ipologist was always emitting the same ip address

--- heimdall/doorway/src/main/java/guru/bonacci/oogway/doorway/ip/IPologist.java
+++ heimdall/doorway/src/main/java/guru/bonacci/oogway/doorway/ip/IPologist.java
@@ -1,25 +1,26 @@
 package guru.bonacci.oogway.doorway.ip;
 
+import java.util.Iterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 @Profile("!prod")
 public class IPologist implements IIPologist {
 
-	private Flux<String> iperator;
-
+	private Iterator<String> iperator;
+	
 	@Autowired
 	public IPologist(IPerable iperable) {
-		iperator = Flux.fromIterable(iperable);
+		iperator = iperable.iterator();
 	}
 
 	@Override
-	public Mono<String> checkUp(String ipIn) {
-		return iperator.next();
+	public Mono<String> checkUp(String ip) {
+		return Mono.defer(() -> Mono.just(iperator.next()));
 	}
 }

b0435132c9fbc636ada8b8f3a5fc5e7efbe00336
WhoAm2 <jvh1234@outlook.com>
2018-12-18 17:16:29 +0100

    join kstream-ktable

--- enricher/src/main/java/guru/bonacci/oogway/enricher/EnrichApp.java
+++ enricher/src/main/java/guru/bonacci/oogway/enricher/EnrichApp.java
@@ -4,6 +4,8 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Serialized;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -39,18 +41,17 @@ public class EnrichApp {
 		@StreamListener
 		public void process(@Input(Binding.QUOTE) KStream<String, EnquiryEvent> quotes,
 							@Input(Binding.ENRICH) KStream<String, Enrichment> riches) {
-			quotes.print("q");
-			riches.print("r");
+
+			KTable<String, Enrichment> enrichTable = 
+					riches.groupByKey(Serialized.with(Serdes.String(), new EnrichmentSerde())).reduce(
+							(aggValue, newValue) -> newValue);			
+
 			KStream<?, BigGem> leftJoin = 
-					quotes.leftJoin(riches, 
+					quotes.leftJoin(enrichTable, 
 							(q, r) ->  new BigGem(q, r), 
-							JoinWindows.of(5000),
 							Joined.with(Serdes.String(), 
 									new EnquiryEventSerde(),
 									new EnrichmentSerde()));
-			leftJoin.print("joint");
-			System.out.println("stop");
-			
 		}
 	}
 	

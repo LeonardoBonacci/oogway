32864076f8bcf8180b0aef4fb6da97c881b9acf0
WhoAm2 <jvh1234@outlook.com>
2018-12-19 09:18:16 +0100

    doorway-oracle can read biggem

--- enricher/src/main/java/guru/bonacci/oogway/enricher/BigGem.java
+++ enricher/src/main/java/guru/bonacci/oogway/enricher/BigGem.java
@@ -2,18 +2,21 @@ package guru.bonacci.oogway.enricher;
 
 import java.io.Serializable;
 
-import guru.bonacci.oogway.domain.EnquiryEvent;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class BigGem implements Serializable {
 
 	private static final long serialVersionUID = -2926410041404429201L;
 
-	private EnquiryEvent e1;
-	private Enrichment e2;
+	public BigGem(String saying, String author, Enrichment data) {
+		this.saying = saying;
+		this.author = author;
+		this.extra = data;
+	}
+	
+	private String saying, author;
+	private Enrichment extra;
 }
--- enricher/src/main/java/guru/bonacci/oogway/enricher/EnrichApp.java
+++ enricher/src/main/java/guru/bonacci/oogway/enricher/EnrichApp.java
@@ -1,7 +1,9 @@
 package guru.bonacci.oogway.enricher;
 
+import static java.util.UUID.randomUUID;
+
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.kstream.JoinWindows;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
@@ -10,49 +12,61 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 import guru.bonacci.oogway.domain.EnquiryEvent;
 
-
 @SpringBootApplication
 public class EnrichApp {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(EnrichApp.class, args);
 	}
-	
+
 	interface Binding {
 
-		String ENRICH = "enrich";
 		String QUOTE = "quote";
+		String ENRICH = "enrich";
+		String BIGGEM = "biggem";
+
+		@Input(QUOTE)
+		KStream<?, ?> quote();
 
 		@Input(ENRICH)
 		KStream<?, ?> enrich();
 
-		@Input(QUOTE)
-		KStream<?, ?> quote();
+		@Output(BIGGEM)
+		KStream<?, ?> biggem();
 	}
-
 
 	@EnableBinding(Binding.class)
 	public static class Processor {
 
+		@SuppressWarnings("deprecation")
+		@SendTo(Binding.BIGGEM)
 		@StreamListener
-		public void process(@Input(Binding.QUOTE) KStream<String, EnquiryEvent> quotes,
-							@Input(Binding.ENRICH) KStream<String, Enrichment> riches) {
+		public KStream<String, BigGem> process(
+				@Input(Binding.QUOTE) KStream<String, EnquiryEvent> quotes,
+				@Input(Binding.ENRICH) KStream<String, Enrichment> enrich) {
+			
+			quotes.print("quote");
 
-			KTable<String, Enrichment> enrichTable = 
-					riches.groupByKey(Serialized.with(Serdes.String(), new EnrichmentSerde())).reduce(
-							(aggValue, newValue) -> newValue);			
+			KTable<String, Enrichment> enrichTable = enrich
+					.groupByKey(Serialized.with(Serdes.String(), new EnrichmentSerde()))
+					.reduce((aggValue, newValue) -> newValue);
+			enrichTable.print("enrichment data");
 
-			KStream<?, BigGem> leftJoin = 
-					quotes.leftJoin(enrichTable, 
-							(q, r) ->  new BigGem(q, r), 
-							Joined.with(Serdes.String(), 
-									new EnquiryEventSerde(),
-									new EnrichmentSerde()));
+			KStream<String, BigGem> join = quotes.leftJoin(enrichTable, 
+					(q, r) -> new BigGem(q.getQuestion(), q.getApikey(), r),
+					Joined.with(Serdes.String(), new EnquiryEventSerde(), new EnrichmentSerde()));
+
+			KStream<String, BigGem> output = 
+					join.map((k, v) -> new KeyValue<>(randomUUID().toString(), v));
+
+			output.print("joined");
+			return output;
 		}
 	}
-	
 }
